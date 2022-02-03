1ffddbd1d978e90dbac9e9524d196c02735fed66
KATALON <KATALON@DESKTOP-EADCNH8>
2022-02-03 16:51:46 +0700

    my frst commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4d2f6560-71d2-4237-88d4-e691499494c2</testSuiteGuid>
+   <testCaseLink>
+      <guid>3eac44e2-c370-4743-ad28-836c0f7e6249</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/google</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
